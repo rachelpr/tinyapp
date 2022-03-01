102b95f70059c02fedfcc7537144399e2123b241
Rachel PR <rplourder@gmail.com>
2022-02-28 20:34:50 -0400

    Add route handler for /urls using res.render() to pass URL data to ejs template

--- express_server.js
+++ express_server.js
@@ -2,24 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hey there!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Heeeeey <b>World</b></body></html>\n")
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
