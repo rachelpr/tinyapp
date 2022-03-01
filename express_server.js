3901af4cd27c9e49fbe9410d8a035a8074c441b2
Rachel PR <rplourder@gmail.com>
2022-03-01 19:52:22 -0400

    Add POST route to receive form submission from Urls/new

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const PORT = 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -26,7 +26,10 @@ app.get("/urls:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
+app.post("/urls/", (req, res) => {
+  console.log(req.body);
+  res.send("Okurrr");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
