5ebf88855fbc8ade2575a7129a06c619e9d913f1
Rachel PR <rplourder@gmail.com>
2022-03-01 19:41:26 -0400

    Add body-parser to get ready for POST requests

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
