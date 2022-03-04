ff56912b065a276f20a26ae1111cbe77a7079472
Rachel PR <rplourder@gmail.com>
2022-03-03 22:19:19 -0400

    Added delete functionality to urls/index page

--- express_server.js
+++ express_server.js
@@ -35,7 +35,13 @@ app.post("/urls/", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL)
+  res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
