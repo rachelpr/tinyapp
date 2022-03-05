7a94c7d121fdb986a85c0cf24cc2d7cc4f0c4c63
Rachel PR <rplourder@gmail.com>
2022-03-05 16:03:03 -0400

    Fixed a with urls/new, added in templateVars

--- express_server.js
+++ express_server.js
@@ -24,7 +24,8 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
