df4dcc776204d56eb0c0580d1e27db51cb27a132
Rachel PR <rplourder@gmail.com>
2022-03-06 14:07:30 -0400

    Re-arrange code so that post and get requests are grouped together

--- express_server.js
+++ express_server.js
@@ -7,7 +7,6 @@ const PORT = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -28,6 +27,25 @@ const users = {
   }
 }
 
+const generateRandomString = function () {
+  let randomString = ""
+  let chars = "abcdefghijklmnopqrstufwxyzABCDEFGHIJKLMNOPQRSTUFWXYZ1234567890"
+  let charLength = chars.length
+  for (let i = 0; i < 6; i++) {
+    randomString += chars.charAt(Math.random() * charLength);
+  }
+  return randomString
+}
+
+const generateRandomID = function () {
+  let randomID = ""
+  let chars = "1234567890"
+  let charLength = chars.length
+  for (let i = 0; i < 8; i++) {
+    randomID += chars.charAt(Math.random() * charLength);
+  }
+  return randomID
+}
 
 app.get("/urls", (req, res) => {
   const templateVars = {
@@ -58,6 +76,16 @@ app.post("/urls/", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = { user: users[req.cookies["userID"]] }
+  res.render("urls_register", templateVars)
+});
+
 app.post("/register", (req, res) => {
   const userID = generateRandomID();
   const userEmail = req.body.email
@@ -72,10 +100,6 @@ app.post("/register", (req, res) => {
   res.redirect("/urls")
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
-});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -100,10 +124,6 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/register", (req, res) => {
-  const templateVars = { user: users[req.cookies["userID"]] }
-  res.render("urls_register", templateVars)
-});
 
 
 
@@ -111,23 +131,5 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
 
-const generateRandomString = function () {
-  let randomString = ""
-  let chars = "abcdefghijklmnopqrstufwxyzABCDEFGHIJKLMNOPQRSTUFWXYZ1234567890"
-  let charLength = chars.length
-  for (let i = 0; i < 6; i++) {
-    randomString += chars.charAt(Math.random() * charLength);
-  }
-  return randomString
-}
 
-const generateRandomID = function () {
-  let randomID = ""
-  let chars = "1234567890"
-  let charLength = chars.length
-  for (let i = 0; i < 8; i++) {
-    randomID += chars.charAt(Math.random() * charLength);
-  }
-  return randomID
-}
 
