cb2263ecc185a04a0fd2355fe769516fe4298147
Rachel PR <rplourder@gmail.com>
2022-03-11 20:24:38 -0400

    Add a test for invalid user

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -18,6 +18,11 @@ describe("getUserWithEmail", function () {
   it("should return a user with a valid email", function () {
     const user = getUserWithEmail("user@example.com", testUsers)
     const expectedID = "userRandomID";
-    assert.equal(user["id"],expectedID);
+    assert.equal(user["id"], expectedID);
   });
+  it("should return undefined with a user not in users database", function () {
+    const user = getUserWithEmail("nonUser@example.com", testUsers)
+    assert.equal(user, undefined)
+  })
 });
+
