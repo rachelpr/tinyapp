4a6ccf07b7f53bd41d4bf707f022e7269f648e8c
Rachel PR <rplourder@gmail.com>
2022-03-11 20:14:06 -0400

    Add a test folder and helpers test file to test the helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,23 @@
+const { assert } = require("chai");
+
+const { getUserWithEmail } = require("../helpers");
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-lifted-truck"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishes-stink"
+  }
+}
+describe("getUserWithEmail", function () {
+  it("should return a user with a valid email", function () {
+    const user = getUserWithEmail("user@example.com", testUsers)
+    const expectedID = "userRandomID";
+    assert.equal(user["id"],expectedID);
+  });
+});
